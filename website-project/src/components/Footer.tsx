d84685768b7c28e7c251031dbd44085ae29cd75a
Alper Beşli <alper5li@users.noreply.github.com>
2023-02-28 01:44:10 +0300

    unnecessary things deleted

--- website-project/src/components/Footer.tsx
+++ website-project/src/components/Footer.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='bg-dark mt-auto'>
-            <div className='container'>
-                <div className="row text-white">
-                    <div className="col-md-4 mb-3">
-                        <h5>Anasayfa</h5>
+            <div className='container d-flex justify-content-center'>
+                <div className="row  text-white">
+                    <div className="col-md-4 mx-2 ">                   
+                        <a href="www.google.com">Anasayfa</a>
                     </div>
-                    <div className="col-md-4 mb-3">
-                        <h5>Hakkımızda</h5>
+                    <div className="col-md-4 mx-2  ">
+                        <a href="www.google.com">Hakkımızda</a>
                     </div>
-                    <div className="col-md-4 mb-3">
-                        <h5>İletişim</h5>
+                    <div className="col-md-4   ">
+                        <a href="www.google.com">İletişim</a>
                     </div>
                 </div>
             </div>
