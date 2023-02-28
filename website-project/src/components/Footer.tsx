6d1a8cee3f1c10721ce19581421abac2e7a434b9
Alper Beşli <alper5li@users.noreply.github.com>
2023-03-01 01:45:00 +0300

    .

--- website-project/src/components/Footer.tsx
+++ website-project/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import svgList from '../helpers/svgList'
-
+import '../css/Footer.css'
 const svg = svgList();
 
 
@@ -14,29 +14,35 @@ export default function Footer() {
         <div className="container flex p-4 pb-0">
          <section className="mb-4 flex justify-content-evenly">
          
-            <a className="btn btn-outline-light btn-floating m-4 bg-white "  href="#!" role="button">
-                <img className="text-center rounded" src={svg["Whatsapp"]} alt="" />  
+            <a className="btn btn-floating btn-block m-4 bg-dark equalButton"  href="#!" role="button">
+                <img className="text-center rounded b-svg" src={svg["Whatsapp"]} alt="" />  
             </a>
             
-            <a className="btn btn-outline-light btn-floating m-4 bg-white" href="#!" role="button">   
-                <img className="text-center rounded" src={svg["Facebook"]} alt="" />
-
-            </a>
-            
-            <a className="btn btn-outline-light btn-floating m-2 bg-white" href="#!" role="button">
-                <img className="text-center rounded" src={svg["Twitter"]} alt="" />
-
-             </a>
-             
-            <a className="btn btn-outline-light btn-floating m-4 bg-white" href="#!" role="button">
-                <img className="text-center rounded" src={svg["Airplane"]} alt="" />
+            <a className="btn btn-floating  btn-block m-4 bg-dark equalButton" href="#!" role="button">   
+                <img className="text-center rounded b-svg" src={svg["Facebook"]} alt="" />
 
             </a>
             
-            <a className="btn btn-outline-light btn-floating m-4 bg-white" href="#!" role="button">
-                <img className="text-center rounded" src={svg["Phone"]} alt="" />
+            <a className="btn btn-floating btn-block m-2 bg-dark equalButton" href="#!" role="button">
+                <img className="text-center rounded b-svg" src={svg["Twitter"]} alt="" />
 
-            </a>            
+            </a>         
+            
+            <a className="btn btn-floating m-4 bg-dark equalButton" href="#!" role="button">
+                <img className="text-center rounded b-svg" src={svg["Phone"]} alt="" />
+
+            </a>
+
+            <a className="btn btn-floating m-4 bg-dark equalButton" href="#!" role="button">
+                <img className="text-center rounded b-svg" src={svg["Instagram"]} alt="" />
+
+            </a>
+
+            <a className="btn btn-floating m-4 bg-dark equalButton" href="#!" role="button">
+                <img className="text-center rounded b-svg" src={svg["Youtube"]} alt="" />
+
+            </a>      
+
             </section>
   </div>
 
--- website-project/src/helpers/svgList.tsx
+++ website-project/src/helpers/svgList.tsx
@@ -1,9 +1,10 @@
 import Airplane from '../svg/airplane.svg'
 import Facebook from '../svg/facebook.svg'
 import Twitter from '../svg/twitter.svg'
-import Phone from '../svg/telephone-fill.svg'
+import Phone from '../svg/phone.svg'
 import Whatsapp from '../svg/whatsapp.svg'
-
+import Youtube from '../svg/youtube.svg'
+import Instagram from '../svg/instagram.svg'
 
 
 let svgs:any = [];
@@ -14,7 +15,8 @@ svgs["Facebook"] = Facebook;
 svgs["Twitter"] = Twitter;
 svgs["Phone"] = Phone;
 svgs["Whatsapp"] = Whatsapp;
-
+svgs["Youtube"] = Youtube;
+svgs["Instagram"] = Instagram;
 
 
 export default function svgList() {
